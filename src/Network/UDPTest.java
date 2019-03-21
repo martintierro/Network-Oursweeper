997ea64d137ce4cea9105469f1ee32dacfdc6d70
martintierro <38676234+martintierro@users.noreply.github.com>
2019-03-21 16:43:28 +0800

    Added testing network files

---
+++ src/Network/UDPTest.java
@@ -0,0 +1,13 @@
+package Network;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+public class UDPTest {
+    OurSweeperClient client;
+
+    public static void main(String[] args){
+
+    }
+
+}
