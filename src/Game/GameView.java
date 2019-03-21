fa42b5a76568e2cca022ba99791910558b763d67
schuyler <schuyler_ng@dlsu.edu.ph>
2019-03-21 12:02:03 +0800

    updated again

--- src/Game/GameView.java
+++ src/Game/GameView.java
@@ -1,9 +1,36 @@
 package Game;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
 public class GameView extends View{
 
     @Override
     public void update() {
 
     }
+
+    /*
+    TilePane tilePane = new TilePane();
+
+
+    for(int i = 0; i < 100; i++){
+        Image dirt = new Image("/Pictures/grass.png");
+        ImageView dirtTile = new ImageView(dirt);
+        dirtTile.setFitWidth(30.0);
+        dirtTile.setFitHeight(30.0);
+
+        Button button = new Button();
+        button.setGraphic(dirtTile);
+        button.setPrefSize(50.0, 50.0);
+        tilePane.getChildren().add(button);
+    }
+
+    Scene scene = new Scene(tilePane, 450,450);
+
+    */
 }
--- src/Game/LoginView.java
+++ src/Game/LoginView.java
@@ -5,15 +5,23 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginView extends View {
+import java.awt.event.ActionEvent;
 
-    @FXML
-    Label usernameLabel, passwordLabel, gameLabel;
-    TextField usernameTextField, passwordTextField;
-    Button playButton;
+public class LoginView extends View {
 
     @Override
     public void update() {
 
+    }
+
+    @FXML
+    Label usernameLabel, passwordLabel, gameLabel;
+    TextField usernameTextField, addressTextField;
+    Button playButton;
+
+    public void clickPlay(ActionEvent actionEvent) throws Exception{
+        System.out.println("Play Button Clicked!");
+        System.out.println("Username: " +usernameTextField.getText());
+        System.out.println("IP Address: " +addressTextField.getText());
     }
 }
