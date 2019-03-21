d257dcb83548107fc9bd4d0b378ab7b8419546b9
martintierro <38676234+martintierro@users.noreply.github.com>
2019-03-21 23:21:51 +0800

    Added sweepNextTile to GameController

--- src/Game/GameController.java
+++ src/Game/GameController.java
@@ -1,7 +1,25 @@
 package Game;
 
-import java.util.ArrayList;
-
 public class GameController {
     private GameModel game;
+
+    public GameController(GameModel game) {
+        this.game = game;
+    }
+
+    public void sweepNextTile(int tileIndex){
+        Tile tile = game.getField().getTile(tileIndex);
+        tile.setSweep(true);
+        if(tile.isBomb())
+            game.getCurrentPlayer().setAlive(false);
+        int nextPlayerIndex = (game.getPlayers().indexOf(game.getCurrentPlayer())+1);
+        if(nextPlayerIndex>=game.getPlayers().size())
+            nextPlayerIndex = 0;
+        while(!game.getPlayers().get(nextPlayerIndex).isAlive())
+           nextPlayerIndex++;
+        Player nextPlayer = game.getPlayers().get(nextPlayerIndex);
+        game.setCurrentPlayer(nextPlayer);
+        game.notifyViews();
+    }
+
 }
