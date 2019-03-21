b3e1c397c6c28da6e9806289a3dca6707237379e
martintierro <38676234+martintierro@users.noreply.github.com>
2019-03-21 23:59:43 +0800

    Added game state

--- src/Game/GameState.java
+++ src/Game/GameState.java
@@ -1,12 +1,41 @@
 package Game;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class GameState implements Serializable {
 
     private Player currentPlayer;
+    private ArrayList<Integer> sweepedTileIndeces;
+    private boolean over;
+    private ArrayList<Integer> alivePlayers;
 
     public GameState(GameModel game){
+        sweepedTileIndeces = new ArrayList<>();
+        alivePlayers = new ArrayList<>();
+        currentPlayer = game.getCurrentPlayer();
+        for(Tile tile:game.getField().getTiles())
+            if(tile.isSweep())
+                sweepedTileIndeces.add(game.getField().getTiles().indexOf(tile));
+        over = game.checkIfOver();
+        for(Player player:game.getPlayers())
+            if(player.isAlive())
+                alivePlayers.add(game.getPlayers().indexOf(player));
     }
 
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public ArrayList<Integer> getSweepedTileIndeces() {
+        return sweepedTileIndeces;
+    }
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public ArrayList<Integer> getAlivePlayers() {
+        return alivePlayers;
+    }
 }
