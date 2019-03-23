0135c0e47cc9f50c2ac79bae2865cc401b9920d2
martintierro <38676234+martintierro@users.noreply.github.com>
2019-03-23 17:14:51 +0800

    Added sendPacket, receiveState

--- src/Network/UDPClient.java
+++ src/Network/UDPClient.java
@@ -1,9 +1,39 @@
 package Network;
 
+import Game.GameState;
+
 import java.io.*;
 import java.net.*;
 
 public class UDPClient {
+    private DatagramSocket clientSocket;
+    private InetAddress serverIPAddress;
+
+    public UDPClient(InetAddress serverIPAddress) {
+        this.serverIPAddress = serverIPAddress;
+        try {
+            clientSocket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendPacket(Integer i) throws Exception{
+        byte[] sendData = new byte[1024];
+        sendData[0] = i.byteValue();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIPAddress, 9876);
+        clientSocket.send(sendPacket);
+    }
+
+    public GameState receiveState() throws Exception{
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket (receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        //TODO return game state
+        return null;
+    }
+
+
     public static void main(String args[]) throws Exception {
         BufferedReader inFromUser =
                 new BufferedReader(new InputStreamReader(System.in));
