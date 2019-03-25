e49d2738c8b1544ab15b0adc98ec7077dc9899a7
martintierro <38676234+martintierro@users.noreply.github.com>
2019-03-25 15:49:17 +0800

    Changed Bomb density

--- src/Game/Field.java
+++ src/Game/Field.java
@@ -12,7 +12,7 @@ public class Field implements Serializable{
         tiles = new ArrayList<>();
         for(int i=0 ; i<100 ; i++){
             tiles.add(new Tile());
-            if(random.nextInt(10)==0)
+            if(random.nextInt(30)==0)
                 this.tiles.get(i).setBomb(true);
         }
     }
